7c9a75f250cc432830c11f49ef6707557c5ea11f
evday <1194984735@qq.com>
2017-12-12 08:26:36 +0800

    增加初始化

--- room/views.py
+++ room/views.py
@@ -1,18 +1,11 @@
 import json
+from datetime import datetime
 
-
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
-
 
 from room import models
 from room.forms import RegisterForm
-
-
-
-
-
-
 
 
 # Create your views here.
@@ -33,6 +26,7 @@ def register(request):
         else:
             registerResponse["error_list"] = form.errors
         return HttpResponse(json.dumps(registerResponse))
+
 
 def login(request):
     if request.method == "GET":
@@ -65,16 +59,77 @@ def login(request):
 
         return HttpResponse(json.dumps(state))
 
+
 def index(request):
+    times = models.Time.objects.all()
+
+    return render(request, 'index.html', {"times": times})
+
+
+def booking(request):
+    """
+    获取会议室预定情况以及预定会议室
+    :param request:
+    :param date:
+    :return:
+    """
+    ret = {'code': 1000, 'msg': None, 'data': None}
+    current_date = datetime.now().date()
+
     if request.method == "GET":
-        times = models.Time.objects.all()
-        rooms = models.Room.objects.all()
-        return render(request,'index.html',{"times":times,"rooms":rooms})
+        try:
+            fetch_date = request.GET.get('date')
+            fetch_date = datetime.strptime(fetch_date, '%Y-%m-%d').date()
+            if fetch_date < current_date:
+                raise Exception('查询时间不能是以前的时间')
+
+            booking_list = models.Order.objects.filter(day=fetch_date)
+
+
+            booking_dict = {}
+            for item in booking_list:
+                if item.room_id not in booking_dict:
+                    booking_dict[item.room_id] = {item.time_id: {'name': item.user.user, 'id': item.user_id}}
+                else:
+                    if item.time_id not in booking_dict[item.room_id]:
+                        booking_dict[item.room_id][item.time_id] = {'name': item.user.user, 'id': item.user_id}
+            """
+            {
+                room_id:{
+                    time_id:{''},
+                    time_id:{''},
+                    time_id:{''},
+                }
+            }
+            """
+
+            room_list = models.Room.objects.all()
+
+            booking_info = []
+            for room in room_list:
+                temp = [{'text': room.name, 'attrs': {'rid': room.id}, 'chosen': False}]
+                for time in models.Time.objects.all():
+                    v = {'text': '', 'attrs': {'time_id': time.id, 'room_id': room.id}, 'chosen': False}
+                    if room.id in booking_dict and time.id in booking_dict[room.id]:
+                        v['text'] = booking_dict[room.id][time.id]['name']
+                        v['chosen'] = True
+                        if booking_dict[room.id][time.id]['id'] != request.session.get('id'):
+                            v['attrs']['disable'] = 'true'
+                    temp.append(v)
+                booking_info.append(temp)
+
+            ret['data'] = booking_info
+        except Exception as e:
+            ret['code'] = 1001
+            ret['msg'] = str(e)
+        return JsonResponse(ret)
+
     elif request.is_ajax():
         state = {"error_list":None,"success":False,"user":None}
         data_list = json.loads(request.body.decode("utf-8"))
+
         user_id = request.session.get("id")
-        print(user_id)
+
 
 
 
@@ -89,24 +144,3 @@ def index(request):
                 print(e)
                 state["error_list"] = "添加失败"
         return JsonResponse(state)
-
-def booking(request):
-    time_list = models.Time.objects.all()
-    rooms = models.Room.objects.all()
-    book_info = models.Order.objects.all()
-    response = []
-    for i in time_list:
-        res = {}
-        room_list = []
-        # attrs =
-        for room in rooms:
-
-            room_list.append(room)
-
-            res["attrs"] = {"room":room.id,"time_id":i.id}
-            response.append(res)
-    print(response)
-    return HttpResponse("ok")
-
-
-
